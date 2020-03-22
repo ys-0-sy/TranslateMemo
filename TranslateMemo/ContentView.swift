916edbb82bb3d829659e37fd97fe788a6624c76c
ys-0-sy <yuta.saito0703@gmail.com>
2020-03-22 21:22:42 +0900

    Initial Commit

---
+++ TranslateMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TranslateMemo
+//
+//  Created by saito on 2020/03/22.
+//  Copyright © 2020 saito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
